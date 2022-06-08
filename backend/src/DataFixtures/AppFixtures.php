9bdd643fb8118c25dffe6e779da031a46f00771f
DnD-Ryu <kevin.lefebvre@dnd.fr>
2022-06-08 14:59:29 +0200

    add some change

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $password = $this->hasher->hashPassword($user, '123456789');
+        $user->setPassword($password);
+        $user->setRoles([
+            'ROLE_ADMIN',
+            'ROLE_USER',
+        ]);
+        $manager->persist($user);
         $manager->flush();
     }
 }
